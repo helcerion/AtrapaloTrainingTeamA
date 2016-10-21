cff14ccef88a0b214eff013590dacff4495ec62c
Marc Rodriguez <marc.rodriguez@atrapalo.com>
2016-10-21 13:53:11 +0200

    Added Board interface

---
+++ src/Board/Board.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace TeamA\Board\Domain;
+
+use TeamA\Piece\Domain;
+
+class Board{
+
+    public function addPiece(Piece p) {
+    }
+
+    public function movePieceDown() {
+    }
+
+    public function movePieceLeft() {
+    }
+
+    public function movePieceRight() {
+    }
+
+    public function rotatePieceCW() {
+    }
+
+    public function rotatePieceCCW() {
+    }
+
+}
