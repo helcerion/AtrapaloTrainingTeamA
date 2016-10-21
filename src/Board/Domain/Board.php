c532940feab27f114b7d5112439e7435281d158e
Marc Rodriguez <marc.rodriguez@atrapalo.com>
2016-10-21 14:47:52 +0200

    Implementing bounding boxes in board

--- src/Board/Domain/Board.php
+++ src/Board/Domain/Board.php
@@ -8,12 +8,27 @@ use TeamA\Point\Domain;
 
 class Board{
 
+    const HEIGHT = 20;
+    const WIDTH = 10;
+
     private $pieces;
     private $actualPiece;
     private $hasActualPiece;
 
+    private $boundingBox;
+
     public function Board() {
+        // Initialize bounding box
+        $bb = new BoundingBox();
+        $pointBuilder = new PointBuilder();
+        $leftTop = $pointBuilder->build(0, 0);
+        $rightBottom = $pointBuilder->build(WIDTH, HEIGHT);
+        $bb->setLeftTop($leftTop);
+        $bb->setRightBottom($rightBottom);
+
+        // Initialize attrs
         $this->pieces = array();
+        $this->boundingBox = $bb;
     }
 
     public function addPiece(Piece $p) {
@@ -25,6 +40,19 @@ class Board{
         $leftTop = $boundingBox->getLeftTop();
         $rightBottom = $boundingBox->getRightBottom();
 
+        $leftTop->increaseX($x);
+        $leftTop->increaseY($y);
+        $rightBottom->increaseX($x);
+        $rightBottom->increaseY($y);
+
+        $newBb = new BoundingBox();
+        $newBb->setLeftTop($leftTop);
+        $newBb->setRightBottom($rightBottom);
+
+        return isInside($newBb, $boundingBox);
+    }
+
+    private function isInside(BoundingBox $a, BoundingBox $b) {
 
     }
 
